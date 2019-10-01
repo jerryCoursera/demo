dbc7ec22d1b5f3a907349fe39bbc503e94f24204
jerry-ban <One00001>
2019-10-01 16:55:32 -0400

    add process parameter while running flask

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ import os
 port_number = int(os.getenv("PORT_NUMBER", 8831))
 app = connexion.FlaskApp(__name__, specification_dir='openapi/')
 app.add_api('my_api.yaml', arguments={'title': 'Optimizion Demo Service'})
-app.run(port=port_number, debug=True)
+app.run(processes = 3, port=port_number, debug=True)
